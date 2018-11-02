0e4cafd0a149d3563dbba94774aa2a12ab1eaf35
unknown <jodimcarlson4@gmail.com>
2018-11-01 20:15:50 -0500

    worked on code

--- javascript.js
+++ javascript.js
@@ -1,7 +1,8 @@
 
-var FirstTrain
-var nextTrain
-var minutesUnitlTrain  
+var FirstTrain;
+var nextTrain;
+var minutesUnitlTrain;  
+var Frequency;
 
   var config = {
     apiKey: "AIzaSyAkrnkgBC47E4rC04jHzHL1c6fN4dAHRqI",
@@ -22,16 +23,16 @@ var minutesUnitlTrain
     var TrainName = $("#Train").val().trim();
   var Destination = $("#destination").val().trim();
   
-   FirstTrain = moment($("#time").val().trim()).format("HH:mm:ss");
-  //console.log(FirstTrain);
-    var Frequency = $("#Frequency").val().trim();
+   FirstTrain = moment($("#time").val().trim(), "HH:mm" ).subtract(10,"years").format("X");
+  console.log(FirstTrain + "tresting");
+     Frequency = $("#Frequency").val().trim();
 
 var NewTrain = {
     TrainName : TrainName,
     Destination : Destination,
     FirstTrain: FirstTrain,
     Frequency : Frequency
-}; //console.log(NewTrain);
+}; console.log(NewTrain);
   
 database.ref().push(NewTrain);
 
@@ -43,53 +44,58 @@ $("#Train").val("");
   });
 
   database.ref().on("child_added", function (addTrain){
-    var TableDiv = $("<tr>");
+   //console.log(childSnapshot.val());
+  var anotherTrain = addTrain.val().TrainName;
+  //console.log(anotherTrain + "ooops");
+  var anotherLocation = addTrain.val().Destination;
+  var anotherMinutesUntilTrain = addTrain.val().minutesUnitlTrain;
+  var anotherHowOften = addTrain.val().Frequency;
+  var anotherFirstTrain = addTrain.val().FirstTrain;
+  // var differentTime = moment().diff(moment.unix(anotherTrain), 'mm');
+   var remainingTime = moment().diff(moment.unix(anotherFirstTrain), 'mm')% anotherHowOften;
+   minutesUnitlTrain = anotherHowOften-remainingTime;
+   //console.log(minutesUnitlTrain + "OOOPPS");
+  // console.log(differentTime);
+  var arrivalOfNextTrain = moment().add(minutesUnitlTrain, 'm').format("HH:mm A");
+  console.log(arrivalOfNextTrain + 'hope!!');
+   var TableDiv = $("<tr>");
 
   
     var Train = $("<td>" + addTrain.val().TrainName + "</td>");
    var location = $("<td>" + addTrain.val().Destination + "</td>");
-    minutesUnitlTrain = $("<td>" + addTrain.val().minutesUnitlTrain + "</td>");
+    var minutesUnitlTrainTD = $("<td>" + minutesUnitlTrain + "</td>");
     var howOften = $("<td>" + addTrain.val().Frequency + "</td>");
     nextTrain = $("<td>" + addTrain.val().nextTrain + "</td>");
 
-    TableDiv.append(Train).append(location).append(howOften).append(nextTrain).append(minutesUnitlTrain);
+    TableDiv.append(Train).append(location).append(howOften).append(nextTrain).append(minutesUnitlTrainTD);
     $("#table").append(TableDiv);
   });
 
-  var trainFrequency = 5;
+  
 
-  var firstTime = "02:30";
 
-  var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
-console.log(firstTimeConverted);
 
+  var firstTimeConverted = moment(FirstTrain, "HH:mm").subtract(1, "years");
+console.log(firstTimeConverted + "test");
+console.log(FirstTrain + "check");
 var currentTime = moment();
 console.log("CURRENT TIME : " + moment(currentTime).format("HH:mm"));
 
-var differenceTime = moment().diff(moment(firstTimeConverted), "minutes");
+var differenceTime = firstTimeConverted - currentTime.format("mm");
 console.log("DIFFERENCE IN TIME " + differenceTime);
 
-var remainder = differenceTime % trainFrequency;
+var remainder = differenceTime % Frequency;
 console.log(remainder);
 
-minutesUnitlTrain = trainFrequency - remainder;
-console.log(minutesUnitlTrain);
 
- nextTrain = moment().add(minutesUnitlTrain, "minutes");
+
+ nextTrain = currentTime.add(minutesUnitlTrain).format("mm");
 console.log("ARRIVAL TIME " + moment(nextTrain).format("hh:mm"));
 
 
 
  
 
-  //   //do calculation for next arrival time
-  //   //do calculation for minutes till next train arrives
-
+           
     
-
-  //    // $("<td>").text(add var from above calculation section),
-  //      //  $("<td>").text(add var from above calculation section),
-  //             );
-              
-    
-  //  });
+  
